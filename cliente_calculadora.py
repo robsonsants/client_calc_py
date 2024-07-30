96a50451f3c254a77b442f792db6d2fd45d9352f
Robson Santos <allrobson99@gmail.com>
2024-07-29 22:09:50 -0300

    Pequenas alterações nos comentários

--- cliente_calculadora.py
+++ cliente_calculadora.py
@@ -3,7 +3,7 @@ import requests
 # URL do servidor
 BASE_URL = "https://calculadora-fxpc.onrender.com"
 
-# Função para listar operações disponíveis
+# Função para listar todas as operações disponíveis no servidor
 def listar_operacoes():
     response = requests.get(f"{BASE_URL}/operations")
     print(f"Request Status Code (listar_operacoes): {response.status_code}")
@@ -12,7 +12,7 @@ def listar_operacoes():
     else:
         raise Exception("Erro ao listar operações")
 
-# Função para realizar uma operação
+# Função para realizar uma operação ( que pode ser soma, subtração, multiplicação e divisão)
 def realizar_operacao(operacao, param1, param2):
     path = f"/operation/{operacao}/{param1}/{param2}"
     response = requests.post(f"{BASE_URL}{path}")
@@ -23,31 +23,25 @@ def realizar_operacao(operacao, param1, param2):
     else:
         raise Exception(f"Erro ao realizar operação {operacao}: {response.text}")
 
-# Função principal para executar o código
+# Função principal para execução do código
 def main():
     try:
-        # Listar operações disponíveis
         operacoes = listar_operacoes()
         print("Operações disponíveis:", operacoes)
 
-        # Realizar uma operação (exemplo: soma de 5 + 3)
         resultado = realizar_operacao("soma", 5, 3)
         print("Resultado da soma:", resultado)
 
-        # Realizar uma operação (exemplo: subtração de 10 - 4)
         resultado = realizar_operacao("subtracao", 10, 4)
         print("Resultado da subtração:", resultado)
 
-        # Realizar uma operação (exemplo: multiplicação de 6 * 7)
         resultado = realizar_operacao("multiplicacao", 6, 7)
         print("Resultado da multiplicação:", resultado)
 
-        # Realizar uma operação (exemplo: divisão de 20 / 4)
         resultado = realizar_operacao("divisao", 20, 4)
         print("Resultado da divisão:", resultado)
     except Exception as e:
         print(e)
 
-# Executa a função main
 if __name__ == "__main__":
     main()
